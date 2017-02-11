2026a4fbd39cb6345dc70fc171f369e6f1915458
magic-cube <15619225472@163.com>
2017-02-11 23:55:28 +0800

    Hibernate实现

---
+++ src/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,37 @@
+package dao;
+
+import JDBC2.domain.User;
+
+public class UserDaoHibernateImpl implements UserDao {
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User getUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User findUser(String loginName, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
