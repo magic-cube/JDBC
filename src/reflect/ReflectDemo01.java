483889435e898ec4678de568937ca7cf62794d27
magic-cube <15619225472@163.com>
2017-02-11 23:41:51 +0800

    反射获取包括私有方法在内的所有方法和反射获取包括父类在内的所有公共方法,但获取不到私有方法

--- src/reflect/ReflectDemo01.java
+++ src/reflect/ReflectDemo01.java
@@ -1,6 +1,7 @@
 package reflect;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 import JDBC2.domain.User;
 
@@ -11,15 +12,31 @@ import JDBC2.domain.User;
  */
 public class ReflectDemo01 {
 	public static void main (String[] args)throws Exception {
-		User user=(User)creat(User.class);
-		System.out.println(user);
+//		User user=(User)creat(User.class);
+//		System.out.println(user);
+		
+		invoke1(new User());
+		
 	}
 	//传入class对象,构造一个对象
 	static Object creat(Class clazz) throws Exception{
-		//获取构造器
+		//构造器有参数时,获取构造器来创建对象
 		Constructor con=clazz.getConstructor(String.class);
 		//构建对象
 		Object obj=con.newInstance("test name");
 		return obj;
 	}
+	static void invoke1(Object obj){
+		//获取方法,包括私有的,返回方法数组
+		Method[] ms=obj.getClass().getDeclaredMethods();
+		//获取包括父类的方法,但不能获得私有方法
+		ms=obj.getClass().getMethods();
+		for(Method m:ms){
+			System.out.println(m.getName());
+		}
+		
+		
+	}
+	
+	
 }
